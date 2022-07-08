23a67e9e4a15cede0f1ebf45f52d2c35575f0abb
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-07-08 15:03:08 -0300

    Update Projet to work in static production.

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,11 +1,11 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
 import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
-import { eventosAsync } from "./seletores";
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listaDeEventosState",
-  default: eventosAsync,
+  // default: eventosAsync, -- Use Default to load async from DB
+  default: []
 });
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
